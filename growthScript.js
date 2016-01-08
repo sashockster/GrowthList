a16785d4bb68947640f9f51dca75a77c945a10c1
sashockster <shockley.shaina@gmail.com>
2016-01-07 22:44:59 -0800

    spliting js/css file

---
+++ growthScript.js
@@ -0,0 +1,27 @@
+function addItem(){
+var hello=document.getElementById("entry").value;
+	if(hello==""){
+		//if entry is blank then alert
+		alert("This is blank, please try again")
+	}
+	else{
+		addItemFunction(hello)
+	}
+}
+
+function addItemFunction(hello){
+//maybe add an id so when we delete a div, it will delete automatically
+ var html = '<div class="entry"><input type="checkbox" onclick="entryComplete(this)" />'+hello+'<input id="remove-btn" type="image"  src="edit-icon.png" onclick="removeItem(this)"/></div>';
+ $(".container").append(html)
+}
+function removeItem(btnItem){
+    var r = confirm("Are you sure you would like to delete this?");
+	
+    if (r == true) {
+}
+}
+
+function entryComplete(entryName){
+alert("Good Job!")
+	entryName.parentElement.appendTo("#completed");
+}
